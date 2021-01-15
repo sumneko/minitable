deeb1cf936f3c213d04f3e6d0f2a9fdfc7b0b72f
最萌小汐 <sumneko@hotmail.com>
2021-01-15 10:39:11 +0800

    Lua 5.1 的补丁

---
+++ Lua51patch.lua
@@ -0,0 +1,31 @@
+local ipairs       = ipairs
+local getmetatable = getmetatable
+
+local m = {}
+
+---
+---@param t table
+---@return function
+function m.ipairs(t)
+    local mt = getmetatable(t)
+    if mt and mt.__ipairs then
+        return mt.__ipairs(t)
+    else
+        return ipairs(t)
+    end
+end
+
+---
+---@param t table
+---@return integer
+function m.len(t)
+    if type(t) == 'table' then
+        local mt = getmetatable(t)
+        if mt and mt.__len then
+            return mt.__len(t)
+        end
+    end
+    return #t
+end
+
+return m
